7d1e1b1918f2dc024e57e62e4cb9797316a2f0c4
yoktav <iletisimyilmazoktav@gmail.com>
2020-04-24 20:11:13 +0300

    Add header to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Container from '../components/Container/Container';
+import Header from '../components/Header/Header';
 
-const HomePage = () => <Container title="Homepage">Hello</Container>;
+const HomePage = () => (
+  <Container title="Homepage">
+    <Header />
+    Hello
+  </Container>
+);
 
 export default HomePage;
