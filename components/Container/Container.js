911f88ddf48acd469c4fb99d145e1ae257a169b1
yoktav <iletisimyilmazoktav@gmail.com>
2020-04-24 19:32:11 +0300

    Add font

--- components/Container/Container.js
+++ components/Container/Container.js
@@ -9,6 +9,10 @@ const Container = ({ children, title = 'App' }) => (
   <div>
     <Head>
       <title>{title}</title>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      />
     </Head>
     {children}
   </div>
