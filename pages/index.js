d9cd9c83d2ef043f3da1c4f832cd716fbe8dac8d
yoktav <iletisimyilmazoktav@gmail.com>
2020-04-24 21:39:19 +0300

    Add content to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Container from '../components/Container/Container';
 import Header from '../components/Header/Header';
+import Content from '../components/Content/Content';
+import grid from '../node_modules/bootstrap/scss/bootstrap-grid.scss';
 
 const HomePage = () => (
   <Container title="Homepage">
     <Header />
-    Hello
+    <Content className={grid.container}>Hello</Content>
   </Container>
 );
 
