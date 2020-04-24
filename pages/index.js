07ecd03f5d1f3bb4cbc6ab0f4ac1c56b2a76e5e1
yoktav <iletisimyilmazoktav@gmail.com>
2020-04-24 23:02:18 +0300

    Add jumbotron component

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Container from '../components/Container/Container';
 import Header from '../components/Header/Header';
 import Content from '../components/Content/Content';
+import Jumbotron from '../components/Jumbotron/Jumbotron';
 import grid from '../node_modules/bootstrap/scss/bootstrap-grid.scss';
 
 const HomePage = () => (
   <Container title="Homepage">
     <Header />
-    <Content className={grid.container}>Hello</Content>
+    <Content className={grid.container}>
+      <Jumbotron />
+    </Content>
   </Container>
 );
 
