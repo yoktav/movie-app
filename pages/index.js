f05e2811fcce6e751ae4cc38950bf312c8654ead
yoktav <iletisimyilmazoktav@gmail.com>
2020-04-24 23:03:13 +0300

    Add footer to hoempage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import Container from '../components/Container/Container';
 import Header from '../components/Header/Header';
 import Content from '../components/Content/Content';
@@ -10,6 +11,16 @@ const HomePage = () => (
     <Header />
     <Content className={grid.container}>
       <Jumbotron />
+      <p>
+        {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+        Do you want to search other movies?{' '}
+        <Link href="/search">
+          {/* stylelint-disable-next-line scale-unlimited/declaration-strict-value */}
+          <a style={{ color: '#fff' }} href="/search">
+            Go for it!
+          </a>
+        </Link>
+      </p>
     </Content>
   </Container>
 );
